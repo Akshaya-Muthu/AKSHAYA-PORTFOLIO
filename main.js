1bc0242f65d8168c1ea39a87f34d3b3401bd82f7
Akshaya-Muthu <akshayammuthu@gmail.com>
2025-03-23 13:39:48 +0530

    add background image to my personal portfolio and background image also done using html and css

---
+++ main.js
@@ -0,0 +1,67 @@
+ /*================================== toggle icon navbar =================================*/
+
+let nemuIcon = document.querySelector('#menu-icon');
+let navbar =  document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+	menuIcon.classList.toggle('fa-xmark');
+    navbar.classList.toggle('active')
+}
+	
+ /*================================== scroll section active link =================================*/
+
+let sections = document.querySelectionAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+	sections.forEach(sec => {
+		let top = window.scrollY;
+		let offset = sec.offsetTop - 150;
+		let height = sec.offsetHeight;
+		let id = sec.getAtrribute('id');
+		
+		if(top >= offset && top < offset + height) {
+			navLinks.forEach.apply(links => {
+				links.classList.remove('active');
+				document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+			});
+		};
+	});
+ 
+    /*================================== sticky navbar =================================*/
+    let  header = document.querySelector('header');
+	header.classList.toggle('sticky', window.scrollY > 100);
+	
+	/*================================== remove toggle icon and navbar =================================*/
+	menuIcon.classList.remove('fa-xmark');
+	navbar.classList.remove('active');
+};
+	
+/*================================== scroll reveal =================================*/
+ ScrollReveal({
+	 distance: '80px',
+	 duration: 2000'
+	 delay: 200,
+ });
+ 
+ ScrollReveal().reveal('.home-content, heading', { origin: 'top' });
+ ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ ScrollReveal().reveal('.home-contact h1, .about-img', { origin: 'left' });
+ ScrollReveal().reveal('.home-contact p, .about-content', { origin: 'right' });
+ 
+ /*================================== typed js =================================*/
+
+ var typed = new Typed('.multiple-text', {
+    strings: ["Fullstack Developer", "Software Engineer", "Tech Enthusiast", "Freelancer"], 
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+});
+
+
+
+ 
+	 
+ 
+ 
